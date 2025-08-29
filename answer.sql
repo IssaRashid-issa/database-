12ddc88daffd32fdf135633f2e408aacfe1ba61f
IssaRashid-issa <issarashid13822@gmail.com>
2025-08-29 21:12:39 +0300

    week 4

--- answer.sql
+++ answer.sql
@@ -16,7 +16,7 @@ SELECT productName, quantityInStock
 FROM products
 ORDER BY buyPrice ASC
 LIMIT 5;
-
+-- Question 1: Create a table named student
 CREATE TABLE student (
     id INT PRIMARY KEY,
     fullName VARCHAR(100),
@@ -31,3 +31,24 @@ INSERT INTO student (id, fullName, age) VALUES
 UPDATE student
 SET age = 20
 WHERE id = 2;
+
+SELECT paymentDate, SUM(amount) AS totalAmount
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+
+SELECT customerName, country, AVG(creditLimit) AS averageCreditLimit
+FROM customers
+GROUP BY customerName, country;
+
+SELECT productCode, quantityOrdered, (quantityOrdered * priceEach) AS totalPrice
+FROM orderdetails
+GROUP BY productCode, quantityOrdered, priceEach;
+
+SELECT checkNumber, MAX(amount) AS highestAmount
+FROM payments
+GROUP BY checkNumber;
+
+
+
